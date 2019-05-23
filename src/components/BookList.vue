ffa1de57d33cfb2cea0ac5e5bf4924c23b383c4d
Iman Farhanieh <iman.farhanieh@consat.se>
2019-05-23 13:55:43 +0200

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input type="text" placeholder="Sreach Books" v-model="searchInput">
     <ul class="exists">
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books by Ownership</h2>
@@ -48,7 +49,8 @@ export default {
         }
       ],
       filters: ["bought","borrowed"],
-      holding: "bought"
+      holding: "bought",
+      searchInput: ""
     };
   },
   components: {
@@ -58,6 +60,12 @@ export default {
   computed:{
     filteredBooks(){
       return _.filter(this.books, ["ownership", this.holding])
+    },
+    searchedBooks(){
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.ToLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
